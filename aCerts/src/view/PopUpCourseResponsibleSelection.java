df299171e665d78795a70b73182be8f5595f14ef
JonasTvede <Jtvedeh@gmail.com>
2016-05-17 13:46:20 +0200

    Made PopUpCourseResponsibleSelectionList

--- aCerts/src/view/PopUpCourseResponsibleSelection.java
+++ aCerts/src/view/PopUpCourseResponsibleSelection.java
@@ -15,26 +15,26 @@ public class PopUpCourseResponsibleSelection extends Pane {
 
     public PopUpCourseResponsibleSelection()
     {
-        //Creating the two buttons the label.
+        //Create the two buttons the label.
         addNewCourseResponsibleButton = new Button("New Course Responsible");
         chooseExistingCourseResponsibleButton = new Button("Existing Course Responsible");
         instructions = new Label("Please select whether you want an existing or to add a new course responsible");
 
-        //Setting addNewCourseResponsibleButton's coordinates and size.
+        //Set addNewCourseResponsibleButton's coordinates and size.
         addNewCourseResponsibleButton.setLayoutX(50);
         addNewCourseResponsibleButton.setLayoutY(50);
         addNewCourseResponsibleButton.setPrefSize(200, 100);
 
-        //Setting chooseExistingCourseResponsibleButton's coordinates and size.
+        //Set chooseExistingCourseResponsibleButton's coordinates and size.
         chooseExistingCourseResponsibleButton.setLayoutX(270);
         chooseExistingCourseResponsibleButton.setLayoutY(50);
         chooseExistingCourseResponsibleButton.setPrefSize(200, 100);
 
-        //Setting instructions label's coordinates.
+        //Set instructions label's coordinates.
         instructions.setLayoutX(50);
         instructions.setLayoutY(180);
 
-        //Adding it all to the pane.
+        //Add it all to the pane.
         this.getChildren().addAll(addNewCourseResponsibleButton, chooseExistingCourseResponsibleButton, instructions);
     }
 
