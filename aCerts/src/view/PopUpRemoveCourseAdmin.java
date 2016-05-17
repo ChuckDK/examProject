54f3940740c7b1b1667210596946de91e52ad1bb
JonasTvede <Jtvedeh@gmail.com>
2016-05-17 14:51:32 +0200

    Made PopUpRemoveCourseAdmin

--- aCerts/src/view/PopUpRemoveCourseAdmin.java
+++ aCerts/src/view/PopUpRemoveCourseAdmin.java
@@ -14,6 +14,33 @@ public class PopUpRemoveCourseAdmin extends Pane {
     private Button confirmButton;
     private Button cancelButton;
 
-    public PopUpRemoveCourseAdmin() {
+    public PopUpRemoveCourseAdmin()
+    {
+        //Create the text field, the label, and the two buttons.
+        cancelButton = new Button("Cancel");
+        courseIDTextField = new TextField();
+        instructionsLabel = new Label("Please enter Course ID for the course you want to " +
+                "remove\n\t\t\t\t This is permanent");
+        confirmButton = new Button("Confirm");
+
+        //Set the position and the prompt text for the text field.
+        courseIDTextField.setLayoutX(100);
+        courseIDTextField.setLayoutY(100);
+        courseIDTextField.setPromptText("Course ID");
+
+        //Set the position for the instructionsLabel.
+        instructionsLabel.setLayoutX(20);
+        instructionsLabel.setLayoutY(50);
+
+        //Set the position for the confirmButton.
+        confirmButton.setLayoutX(80);
+        confirmButton.setLayoutY(200);
+
+        //Set the position for the cancelButton.
+        cancelButton.setLayoutX(150);
+        cancelButton.setLayoutY(200);
+
+        //Add all the newly created nodes to the pane.
+        this.getChildren().addAll(cancelButton, courseIDTextField, instructionsLabel, confirmButton);
     }
 }
