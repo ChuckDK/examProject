ba83f1c61a26d425ce5440c335461bec36cb9bf4
fcssb <github@forneus.net>
2016-05-17 16:11:13 +0200

    Added PopUpCourseRemovedAdmin

--- aCerts/src/view/PopUpCourseRemovedAdmin.java
+++ aCerts/src/view/PopUpCourseRemovedAdmin.java
@@ -14,5 +14,38 @@ public class PopUpCourseRemovedAdmin extends Pane {
     private Button okayButton;
 
     public PopUpCourseRemovedAdmin() {
+
+        // Create the 3 Labels and the Okay Button.
+        courseIDLabel = new Label("Course ID");
+        courseNameLabel = new Label("Course Name");
+        messageLabel = new Label("Message Label");
+        okayButton = new Button("Okay");
+
+        // Set position for CourseID.
+        courseIDLabel.setLayoutY(100);
+
+        // Set position for CourseName.
+        courseNameLabel.setLayoutY(150);
+
+        // Set position for MessageLabel.
+        messageLabel.setLayoutY(200);
+
+
+        // Set position for Okay button.
+        okayButton.setLayoutX(225);
+        okayButton.setLayoutY(300);
+
+        //Add all the newly created nodes to the pane.
+        this.getChildren().addAll(courseIDLabel, courseNameLabel, messageLabel, okayButton);
+
+        updateLabelPosition(courseIDLabel);
+        updateLabelPosition(courseNameLabel);
+        updateLabelPosition(messageLabel);
     }
+
+    public void updateLabelPosition(Label lbl){
+        float width = com.sun.javafx.tk.Toolkit.getToolkit().getFontLoader().computeStringWidth(lbl.getText(), lbl.getFont());
+        lbl.setLayoutX(250-width/2);
+    }
+
 }
