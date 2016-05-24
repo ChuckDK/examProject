6ff86d61511bd5c86e9729ae5d2541b7d55147bf
MrAsdf <lollolsenxd@gmail.com>
2016-05-24 16:05:30 +0200

    Added comments for FileIO

--- aCerts/src/control/settings/FileIO.java
+++ aCerts/src/control/settings/FileIO.java
@@ -5,17 +5,28 @@ import java.nio.file.Paths;
 
 public class FileIO
 {
+    //Method which will be called upon from other classes to read files with object input stream.
     public static Object readObject(String filename) throws Exception
     {
+        //An object initialisation which have to be here in order to render the method able to be used upon an object.
         Object settingsFile = null;
+
+        //The stream must be set to be null from the start when it are initialized.
         ObjectInputStream in = null;
+
+        //Streaming should be done in a try-catch as it may trigger an exception.
         try{
+            //The object input stream which has the lover types
+            //of input streams and eventually the path of the file in the file input stream.
+            //
             in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(Paths.get("").toAbsolutePath().toString()+"/"+filename)));
             settingsFile = in.readObject();
         }
+        //Handling any exceptions that may occur.
         catch(Exception e){
             e.printStackTrace();
         }
+        //Finally, closing the stream, also using a try catch vs. exception.
         finally{
             try{
                 in.close();
@@ -27,19 +38,27 @@ public class FileIO
         return settingsFile;
     }
 
+    //Method which will be called upon from other classes to write files with object output stream.
     public static void writeObject(String filename, Object file) throws Exception
     {
         ObjectOutputStream out = null;
 
         try
         {
+            //The object output stream which has the lover types of output
+            //streams, writes the result to a new created file with the filename provided in the method scope.
+            //What it writes, is an object.
             out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filename)));
             out.writeObject(file);
         }
+
+        //Handling any exceptions that may occur.
         catch (Exception e)
         {
             e.printStackTrace();
         }
+
+        //Finally, closing the stream, also using a try catch vs. exception.
         finally
         {
             try{
