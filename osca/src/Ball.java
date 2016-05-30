a0d6c387b8bdb130561fb7add4bd71d3dcf313e8
fcssb <github@forneus.net>
2016-05-30 11:48:31 +0200

    Generic

--- osca/src/Ball.java
+++ osca/src/Ball.java
@@ -1,14 +1,18 @@
+import com.sun.javafx.geom.Shape;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
 
-public class Ball implements Runnable {
+public class Ball extends Thread {
 
     private double x, y, deltaX, deltaY, radius;
     private Circle ball;
     private Scene scene;
+    private String debug;
+    private int counter;
 
-    public Ball(double x, double y, double deltaX, double deltaY, double radius, Color color, Scene scene) {
+    public Ball(double x, double y, double deltaX, double deltaY, double radius, Color color, Scene scene, String debug, int counter) {
         this.ball = new Circle(x, y, radius, color);
         this.x = x;
         this.y = y;
@@ -16,6 +20,8 @@ public class Ball implements Runnable {
         this.deltaY = deltaY;
         this.scene = scene;
         this.radius = radius;
+        this.debug = debug;
+        this.counter = counter;
     }
 
 
@@ -49,14 +55,18 @@ public class Ball implements Runnable {
     @Override
     public void run() {
         while (!Thread.interrupted()) {
-        //for (int i = 0; i < 1000; i++) {
-            try {
-                this.moveBall();
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                System.exit(42);
-            }
+            for (int i = 0; i < 100; i++)
+                try {
+                    this.moveBall();
+                    Thread.sleep(100);
+                    //System.out.println(currentThread())
+                    System.out.println(this.debug);
+                    System.out.println(this.counter++);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    //System.out.print(this.ball);
+                    System.exit(1);
+                }
         }
     }
 }
